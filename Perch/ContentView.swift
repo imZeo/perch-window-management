9f4c41c8fbf3f8c0760e7b8cc058a22b74fb344b
imZeo <ZMCsoltai@gmail.com>
2026-03-10 10:47:22 +0100

    Initial Commit

---
+++ Perch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Perch
+//
+//  Created by Zoárd Manó Csoltai on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Perch/PerchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerchApp.swift
+//  Perch
+//
+//  Created by Zoárd Manó Csoltai on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PerchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
